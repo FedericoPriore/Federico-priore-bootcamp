52daab80fec73ab2918f1fe973a617d9550b2d19
FedericoPriore <Federicopriore96@gmail.com>
2024-09-16 20:13:46 -0300

    Nueva version de prueba

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './index.scss'
 import { ProductosProvider } from './context/ProductosContext.jsx'
 import { CarritoProvider } from './context/CarritoContext.jsx'
 
+const endpointLocal = import.meta.env.VITE_LOCAL
+const endpointBackend = import.meta.env.VITE_BACKEND_PRODUCTOS
+
+
 createRoot(document.getElementById('root')).render(
     <ProductosProvider>
         <CarritoProvider>
